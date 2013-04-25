63c5181795e58210a05f7561dd91753cda66afdf
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-04-25 17:58:05 +0100

    if using vim, user has to run :BundleInstall

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,11 @@ sudo apt-get install -y php5-dev
 sudo apt-get install -y vim-nox # vdebug needs it because it has python - default vim doesn't
 sudo apt-get install -y git # vundle needs it
 sudo pecl install xdebug
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 sudo cp /vagrant/xdebug.ini /etc/php5/apache2/conf.d
-sudo cp /vagrant/.vimrc ~
 sudo cp /vagrant/phpinfo.php /var/www
 sudo /etc/init.d/apache2 restart
-sudo vim -c"BundleInstall"
+git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle
+cp /vagrant/.vimrc /home/vagrant
+sudo chown vagrant -R /home/vagrant/.vim
+sudo chown vagrant /home/vagrant/.vimrc
+#vim -c'let $HOME="/home/vagrant" | BundleInstall'
